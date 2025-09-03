ce25ddd48e130ce74371a47b11fda5c172218664
Gerardo Estrella <g.serrato41@outlook.com>
2025-09-03 11:56:57 -0600

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { AuthLayout } from "./auth/layout/AuthLayout";
+import ChatLayout from "./chat/layout/ChatLayout";
+import ChatPage from "./chat/pages/ChatPage";
+
+function App() {
+  return (
+    <ChatLayout>
+      <ChatPage />
+    </ChatLayout>
+  );
+}
+
+export default App;
